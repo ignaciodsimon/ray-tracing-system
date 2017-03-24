b7a267fc3cde54998ddd6d77382e5ab6c9c9ada7
joe <joe@debian>
2017-03-24 10:01:25 -0400

    Added folders and empty files for the basic structure.

---
+++ tracer/compile.sh
@@ -0,0 +1,1 @@
+g++ -o tracer main.cpp tracer.cpp files.cpp 
---
+++ tracer/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "classes.h"
+#include "tracer.h"
+#include "files.h"
+
+int main(int argc, char ** argv)
+{
+	printf("Tracer\n");
+
+	return 0;
+}
+
